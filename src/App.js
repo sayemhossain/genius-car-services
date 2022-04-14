ab30fba7e3cf336cbeb96d235e56b25b70963c6f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-15 02:38:33 +0600

    ne

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Shared/Header/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
