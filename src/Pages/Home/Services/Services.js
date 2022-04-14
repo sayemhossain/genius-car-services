61d0b1a0c6cda7baeb4472f0ed68a2815225a354
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-15 02:33:05 +0600

    new code

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <h4>this is services</h4>
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <Small>copyright @ /dynamic year/</Small>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
