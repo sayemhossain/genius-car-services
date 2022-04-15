bbc018abd330055362fbd2a6bde4c10de6d253b7
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-15 19:21:37 +0600

    navbar section done

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../Img/logo/logo.png";
 const Header = () => {
   return (
-    <div className="bg-primary py-2">
-      <p>this is header</p>
-    </div>
+    <Navbar bg="primary" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand href="#home" className="fw-bold">
+          <img className="w-50" src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Services</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
