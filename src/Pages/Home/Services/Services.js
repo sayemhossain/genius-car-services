14e5da82a99582b88018b5c783857c505c7ba423
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-15 15:07:37 +0600

    new code

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div>
       <h4>this is services</h4>
+      <p>Length: {services.length}</p>
     </div>
   );
 };
