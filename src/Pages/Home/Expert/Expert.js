63df26a633978bc9a5bdfe834fc3e39cddf60aa3
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-15 18:54:29 +0600

    new code

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Expert = () => {
-  return <div></div>;
+const Expert = ({ expert }) => {
+  const { name, img } = expert;
+  return (
+    <div
+      className="card col-12 col-md-6 col-lg-4 mx-auto my-3"
+      style={{ width: "18rem" }}
+    >
+      <img src={img} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          iste molestias porro? Autem, labore?
+        </p>
+        <a href="#" className="btn btn-danger">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Expert;
--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,9 +1,29 @@
 import React from "react";
+import expert1 from "../../../Img/experts/expert-1.jpg";
+import expert2 from "../../../Img/experts/expert-2.jpg";
+import expert3 from "../../../Img/experts/expert-3.jpg";
+import expert4 from "../../../Img/experts/expert-4.jpg";
+import expert5 from "../../../Img/experts/expert-5.jpg";
+import expert6 from "../../../Img/experts/expert-6.png";
+import Expert from "../Expert/Expert";
 
 const Experts = () => {
+  const experts = [
+    { id: 1, name: "Will Smith", img: expert1 },
+    { id: 2, name: "Chris Rock", img: expert2 },
+    { id: 3, name: "Dwayne Mack", img: expert3 },
+    { id: 4, name: "Jhon Snow", img: expert4 },
+    { id: 5, name: "Strak Mello", img: expert5 },
+    { id: 6, name: "Stiven Team", img: expert6 },
+  ];
   return (
-    <div>
-      <h2>Our Experts</h2>
+    <div className="container">
+      <h2 className="my-5 text-center text-uppercase">Our Experts</h2>
+      <div className="row">
+        {experts.map((expert) => (
+          <Expert key={expert.id} expert={expert}></Expert>
+        ))}
+      </div>
     </div>
   );
 };
