45e07a516307f6f0332a5570cc5b63bbc26b19f7
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-20 17:41:01 +0600

    New code added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,18 +1,40 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../../Img/logo/logo.png";
 const Header = () => {
   return (
-    <Navbar bg="primary" variant="dark" sticky="top" expand="md">
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand href="#home" className="fw-bold">
-          <img className="w-50" src={logo} alt="" />
+        <Navbar.Brand href="#home">
+          <Link to="/">
+            <img src={logo} height={30} alt="" />
+          </Link>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Login
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
