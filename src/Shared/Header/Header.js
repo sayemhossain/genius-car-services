3367312f4d55099f20d30803f7ea8414f6cba2fd
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-20 17:46:09 +0600

    New code aded

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto text-center">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
             <Nav.Link as={Link} to="/about">
