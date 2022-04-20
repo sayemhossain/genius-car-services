2c0b2066152495c47c0389d20682483f80b4f821
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-20 17:41:53 +0600

    new code added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
             <Nav.Link as={Link} to="/about">
               About
             </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            <Nav.Link as={Link} to="/login">
               Login
             </Nav.Link>
           </Nav>
