87c8a93ee62caf5cb4e927aa93bcfec888eed771
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-21 17:17:24 +0600

    new code added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Button, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
+import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Register = () => {
   const [createUserWithEmailAndPassword, user] =
@@ -21,51 +22,50 @@ const Register = () => {
   }
 
   return (
-    <div>
-      <div className="container w-50 mx-auto my-5">
-        <h3 className="text-center text-danger mb-4">Register Now</h3>
-        <Form onSubmit={handleRegister}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Your Name</Form.Label>
-            <Form.Control
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              placeholder="Enter email"
-              required
-            />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Register
-          </Button>
-        </Form>
-        <p className="mt-2">
-          Already have an account?
-          <Link to="/login" className="text-danger text-decoration-none">
-            Please Login
-          </Link>
-        </p>
-      </div>
+    <div className="container w-50 mx-auto my-5">
+      <h3 className="text-center text-danger mb-4">Register Now</h3>
+      <Form onSubmit={handleRegister}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+      <p className="mt-2">
+        Already have an account?
+        <Link to="/login" className="text-danger text-decoration-none">
+          Please Login
+        </Link>
+      </p>
+      <SocialLogin></SocialLogin>
     </div>
   );
 };
