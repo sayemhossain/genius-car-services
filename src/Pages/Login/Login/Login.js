1e20fa0260ec284516be278d61564d47891ee327
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-21 17:11:53 +0600

    social media button done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from "react-bootstrap";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
+import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Login = () => {
   const emailRef = useRef("");
@@ -63,6 +64,7 @@ const Login = () => {
           Please Register
         </Link>
       </p>
+      <SocialLogin></SocialLogin>
     </div>
   );
 };
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,42 @@
+import React from "react";
+import google from "../../../Img/logo/google.svg";
+import facebook from "../../../Img/logo/facebook.png";
+import github from "../../../Img/logo/GitHub.png";
+
+const SocialLogin = () => {
+  return (
+    <div>
+      <div className="d-flex align-items-center">
+        <div style={{ height: "1px" }} className="bg-primary w-50"></div>
+        <div className="mx-1 mb-1">or</div>
+        <div style={{ height: "1px" }} className="bg-primary w-50"></div>
+      </div>
+      <div className="my-3">
+        <button className="btn btn-success d-block mx-auto  my-3 w-50">
+          <img className="img-fuild " src={google} alt="" /> Google Sign In
+        </button>
+        <button className="btn btn-primary d-block mx-auto my-3 w-50">
+          <img
+            style={{ width: "25px" }}
+            className="img-fuild me-2 my-1"
+            src={facebook}
+            alt=""
+          />
+          Facebook Sign In
+        </button>
+        <button className="btn btn-dark d-block mx-auto w-50">
+          {" "}
+          <img
+            style={{ width: "25px" }}
+            className="img-fuild me-2 my-1"
+            src={github}
+            alt=""
+          />
+          Github Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
