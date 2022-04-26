f3e70a611a06b95b865d7ef4aeae1f016666859b
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-27 01:32:27 +0600

    new code added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -3,7 +3,19 @@ import { useForm } from "react-hook-form";
 
 const AddService = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    const url = `http://localhost:5000/service`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log(result));
+  };
 
   return (
     <div style={{ height: "90vh" }} className="w-50 mx-auto">
@@ -13,12 +25,12 @@ const AddService = () => {
         <input
           className="mb-2"
           placeholder="Enter name"
-          {...register("Name", { required: true, maxLength: 20 })}
+          {...register("name", { required: true, maxLength: 20 })}
         />
         <textarea
           className="mb-2"
           placeholder="Add a description"
-          {...register("descriotion")}
+          {...register("description")}
         />
         <input
           className="mb-2"
