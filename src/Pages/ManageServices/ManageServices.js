5fd59ca54e69d8e2d29d0e44d6431c244417e9ef
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-27 01:52:15 +0600

    new code added

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -3,12 +3,20 @@ import useServices from "../../hooks/useServices";
 
 const ManageServices = () => {
   const [services] = useServices();
+  const handleDelete = () => {
+    console.log("deleted");
+  };
   return (
     <div className="w-50 mx-auto my-5">
       <h3 className="mb-4 text-uppercase text-danger">Manage Your Services</h3>
       {services.map((service) => (
         <div key={service._id}>
-          <h6>{service.name}</h6>
+          <h6>
+            {service.name}
+            <button className="ms-3 btn btn-danger" onClick={handleDelete}>
+              X
+            </button>
+          </h6>
         </div>
       ))}
     </div>
