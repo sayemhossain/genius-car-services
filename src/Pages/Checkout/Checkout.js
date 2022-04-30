7a6cceb6acd0de950c68a8ea290ff94be1edacde
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-30 23:33:16 +0600

    new code added

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -6,8 +6,56 @@ const Checkout = () => {
   const { serviceId } = useParams();
   const [service] = useServiceDetails(serviceId);
   return (
-    <div>
+    <div className="w-50 mx-auto my-5" style={{ minHeight: "70vh" }}>
       <h4>Please Order:{service.name}</h4>
+      <form action="">
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="name"
+          id=""
+          placeholder="Name"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="email"
+          name="email"
+          id=""
+          placeholder="Enter email"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="service"
+          id=""
+          placeholder="Enter service name"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="address"
+          id=""
+          placeholder="Enter your address"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="phone"
+          id=""
+          placeholder="Phone number"
+          required
+        />
+        <br />
+        <input type="submit" value="Place Order" className="btn btn-primary" />
+      </form>
     </div>
   );
 };
